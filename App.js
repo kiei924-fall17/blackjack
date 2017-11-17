de44673e6e9a74e841b7bb165e4784a74f3e02b1
Brian Eng <brian@brianeng.com>
2017-11-17 14:41:54 -0600

    finished

--- App.js
+++ App.js
@@ -1,23 +1,51 @@
 import React from 'react';
 import shuffledDeck from './ShuffledDeck'
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Image, StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
   constructor() {
     super();
     this.state = {
       deck: [],
-      hand: []
+      hand: ["face_down", "face_down"]
     };
+  }
+  hit() {
+    let newHand = this.state.hand;
+    newHand.push(this.state.deck[0]);
+    let remainingDeck = this.state.deck.slice(1);
+    this.setState({
+      deck: remainingDeck,
+      hand: newHand
+    });
+  }
+  deal() {
+    let newDeck = shuffledDeck();
+    let remainingDeck = newDeck.slice(2);
+    this.setState({
+      deck: remainingDeck,
+      hand: [newDeck[0], newDeck[1]]
+    });
   }
   render() {
     let cards = [];
+    for(let i=0;i<this.state.hand.length;i++) {
+      cards.push(
+        <Image key={i}
+               style={styles.card}
+               source={{uri: "http://golearntocode.com/images/cards/" +  this.state.hand[i] + ".png"}} />
+      );
+    }
     return (
       <View style={styles.container}>
         <View style={styles.cards}>
-          <Text>It'd be awesome if something were here.</Text>
+          {cards}
         </View>
         <View style={styles.buttons}>
+          <Button onPress={() => this.deal()}
+                  title="Let's Play!" />
+          <Button onPress={() => this.hit()}
+                  title="Hit!" />
         </View>
       </View>
     );
@@ -32,17 +60,20 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     justifyContent: 'space-around'
   },
+  // the view that holds the cards
   cards: {
     alignItems: 'center',
     flexDirection: 'row',
     flexWrap: 'wrap',
     justifyContent: 'center'
   },
+  // one card
   card: {
     width: 103,
     height: 150,
     margin: 5
   },
+  // the view that hold the buttons
   buttons: {
     justifyContent: 'flex-end',
     flexDirection: 'row'
